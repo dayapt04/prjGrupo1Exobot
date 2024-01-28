4507bf1026f85eef0ac7e47816a79b2661e04846
Lorena <lopezzelayalore@gmail.com>
2024-01-28 17:43:46 -0500

    Lore

--- src/App.java
+++ src/App.java
@@ -1,6 +1,4 @@
 public class App {
     public static void main(String[] args) {
-        LESoldadoRuso sold1 = new LESoldadoRuso();
-        System.out.println("ID del soldado Ruso: " + sold1.getLeIdSoldado());
     }
 }
