6015a4d01e732a4a17eaec41cf1e1d517566b101
dayapt04 <daya.pt04@gmail.com>
2024-01-29 21:59:14 -0500

    PARTE FINAL

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("\n");
+        System.out.println(" T R A B A J O  G R U P A L  E X O B O T");
+        System.out.println("Integrantes: ");
+        System.out.println("- Lopez Elizabeth");
+        System.out.println("- Pereira Alicia");
+        System.out.println("- Salazar Janeth");
+        System.out.println("- Zuñiga Liam");
+        System.out.println("\n");
+
         AppLZExobot appExobot = new AppLZExobot();
         appExobot.LZAppPrincipalExobot();
     }
